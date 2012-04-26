f322c3fbb0c407aa20a0431a5312a954855d9051
Loic <Loic@Dark-FW-Loic>
2012-04-26 23:03:20 +0200

    Correction fautes d'orthographe

--- js/i18n/fr.js
+++ js/i18n/fr.js
@@ -8,21 +8,21 @@ fr['time'] = 'Durée';
 
 fr['scrollTop'] = 'Remonter';
 
-fr['areYouSure'] = 'Etez-vous sur de vouloir supprimer les élements sélectionnés?';
+fr['areYouSure'] = 'Etes-vous sûr de vouloir supprimer les éléments sélectionnés?';
 
 fr['tliTabLibrary'] = 'Bibliothèque';
 fr['tliTabCurrent'] = 'Liste de lecture';
 fr['tliTabPlaylists'] = 'Playlists';
 fr['tliPreferences'] = 'Préférences';
 
-fr['taction_RefreshArtists'] = 'Rafraichir la liste des artistes';
+fr['taction_RefreshArtists'] = 'Rafraîchir la liste des artistes';
 fr['taction_DecreaseWidth'] = 'Augmenter la largeur';
 fr['taction_IncreaseWidth'] = 'Diminuer la largeur';
 
 fr['tAddToCurrentPlaylist'] = 'Ajouter à la playlist en cours';
 fr['tFavorite'] = 'Favoris';
 fr['tDownload'] = 'Télécharger';
-fr['tAddToFavorite'] = 'Ajouter au favoris';
+fr['tAddToFavorite'] = 'Ajouter aux favoris';
 fr['tPlay'] = 'Jouer';
 fr['tRemove'] = 'Retirer';
 
@@ -30,22 +30,22 @@ fr['action_SelectAll'] = 'Tous';
 fr['action_SelectNone'] = 'Aucun';
 fr['action_AddToPlaylist'] = '+ Playlist';
 fr['action_AddToCurrent'] = '+ En cours';
-fr['action_AddAllToCurrent'] = '+Tous';
-fr['taction_SelectAll'] = 'Tous sélectionner';
+fr['action_AddAllToCurrent'] = '+ Tous';
+fr['taction_SelectAll'] = 'Tout sélectionner';
 fr['taction_SelectNone'] = 'Désélectionner';
 fr['taction_AddToPlaylist'] = 'Ajouter à une playlist';
 fr['taction_AddToCurrent'] = 'Ajouter à la liste de lecture en cours';
-fr['taction_AddAllToCurrent'] = 'Tous ajouter à la liste de lecture en cours';
+fr['taction_AddAllToCurrent'] = 'Tout ajouter à la liste de lecture en cours';
 fr['taction_PlayAlbum'] = 'Jouer l\'album';
 
-fr['allForlder'] = 'Tous les dossier';
+fr['allForlder'] = 'Tous les dossiers';
 
-fr['auto'] = 'Albums automatique';
-fr['newest'] = 'Nouveautées';
+fr['auto'] = 'Albums automatiques';
+fr['newest'] = 'Nouveautés';
 fr['random'] = 'Aléatoire';
 fr['highest'] = 'Mieux notées';
-fr['recent'] = 'Recement jouées';
-fr['frequent'] = 'Fréquement jouées';
+fr['recent'] = 'Récemment jouées';
+fr['frequent'] = 'Fréquemment jouées';
 
 fr['action_Empty'] = 'Vider';
 fr['taction_Shuffle'] = 'Aléatoire';
@@ -55,9 +55,9 @@ fr['taction_Empty'] = 'Tout supprimer';
 fr['action_CurrentSelectAll'] = 'Tous';
 fr['action_CurrentSelectNone'] = 'Aucun';
 fr['action_AddCurrentToPlaylist'] = '+ Playlist';
-fr['taction_CurrentSelectAll'] = 'Tous sélectionner';
+fr['taction_CurrentSelectAll'] = 'Tout sélectionner';
 fr['taction_CurrentSelectNone'] = 'Désélectionner';
-fr['taction_AddCurrentToPlaylist'] = 'Ajouter la séléction à la playlist';
+fr['taction_AddCurrentToPlaylist'] = 'Ajouter la sélection à la playlist';
 
 fr['action_NewPlaylist'] = '+ Nouvelle';
 fr['action_DeletePlaylist'] = 'Supprimer';
@@ -70,17 +70,17 @@ fr['action_RemoveSongs'] = 'Retirer';
 fr['taction_ShufflePlaylist'] = 'Playlist aléatoire';
 fr['taction_RemoveSongs'] = 'Retirer les musiques sélectionnées';
 
-fr['autoplaylist'] = 'Playlists automatique';
+fr['autoplaylist'] = 'Playlist automatique';
 fr['randomPlaylist'] = 'Aléatoire';
 fr['taRandomPlay'] = 'Jouer';
 fr['taRandomAdd'] = 'Ajouter à la playlist en cours';
 
 fr['savePlaylist'] = 'Playlists enregistrées';
 
-fr['ResetPreferences'] = 'Raz';
+fr['ResetPreferences'] = 'RAZ';
 fr['SavePreferences'] = 'Enregistrer';
-fr['tResetPreferences'] = 'Enregistrer les préferences';
-fr['tSavePreferences'] = 'Remettre à zéro les préferences';
+fr['tResetPreferences'] = 'Enregistrer les préférences';
+fr['tSavePreferences'] = 'Remettre à zéro les préférences';
 
 fr['h3Login'] = 'Connexion';
 fr['labelUsername'] = 'Nom d\'utilisateur';
@@ -93,25 +93,25 @@ fr['shortcutsAz'] = '<em>[a-z]</em> Utiliser pour naviguer rapidement dans les a
 fr['shortcutsHome'] = '<em>Home</em> Revenir au début de la liste des artistes';
 fr['shortcutsSpacebar'] = '<em>Spacebar</em> Jouer/Pause';
 fr['shortcutsNextTrack'] = '<em>&rarr;</em> Piste suivante';
-fr['shortcutsPreviousTrack'] = '<em>&larr;</em> Piste précedente';
+fr['shortcutsPreviousTrack'] = '<em>&larr;</em> Piste précédente';
 
 fr['options'] = 'Options';
-fr['labelAutoAlbumSize'] = 'Taille des albums automatique (par défaut 15)';
-fr['labelAutoPlaylistSize'] = 'Taille des playlists automatique (par défaut 25)';
+fr['labelAutoAlbumSize'] = 'Taille des albums automatiques (par défaut 15)';
+fr['labelAutoPlaylistSize'] = 'Taille des playlists automatiques (par défaut 25)';
 fr['labelCacheSize'] = 'Nombre de musiques dans le cache (par défaut 3)';
 fr['labelApplicationName'] = 'Nom de l\'application (Défaut MiniSub)';
-fr['tAutoAlbumSize'] = 'Nombre d\'album à récuperer dans la bibliothèque';
-fr['tAutoPlaylistSize'] = 'Taille des playlists aléatoire';
+fr['tAutoAlbumSize'] = 'Nombre d\'album à récupérer dans la bibliothèque';
+fr['tAutoPlaylistSize'] = 'Taille des playlists aléatoires';
 fr['tApplicationName'] = 'Modifier le nom de l\'application';
 
 fr['labelHideAZ'] = 'Masquer A-Z';
-fr['labelEnableNotifications'] = 'Autoriser les notification (Google Chrome) <small>[Beta]</small>';
-fr['labelScrollTitle'] = 'Defilement du titre';
+fr['labelEnableNotifications'] = 'Autoriser les notifications (Google Chrome) <small>[Beta]</small>';
+fr['labelScrollTitle'] = 'Défilement du titre';
 fr['labelMaxbit'] = 'Débit maximun des musiques';
 fr['unlimitedDebit'] = 'Illmité';
-fr['tHideAZ'] = 'Maquer A-Z en dessous de la liste d\'artistes';
-fr['tEnableNotifications'] = 'Activer les notification lors du changement de musique';
-fr['tScrollTitle'] = 'Faire defiler le titre de la musique';
+fr['tHideAZ'] = 'Masquer A-Z en dessous de la liste d\'artistes';
+fr['tEnableNotifications'] = 'Activer les notifications lors du changement de musique';
+fr['tScrollTitle'] = 'Faire défiler le titre de la musique';
 
 fr['labelLangue'] = 'Langue';
 
@@ -124,7 +124,7 @@ fr['audioLibrary'] = 'Librairie Audio - <a href="http://www.schillmania.com/proj
 
 fr['tChatMsg'] = 'Presser [Entrée] pour envoyer votre message';
 
-fr['divNowPlaying'] = 'Joué en ce moment';
+fr['divNowPlaying'] = 'Ecoutées en ce moment';
 fr['NowPlayingList'] = 'Chargement...';
 fr['divChat'] = '<img src="images/chat_alt_stroke_12x12.png" /> Chat';
 
@@ -132,4 +132,4 @@ fr['tPreviousTrack'] = 'Musique précédente';
 fr['tPlayTrack'] = 'Jouer/Pause';
 fr['tNextTrack'] = 'Musique suivante';
 
-fr['taction_ToggleSideBar'] = 'Montrer la barre latérale';
+fr['taction_ToggleSideBar'] = 'Montrer le menu latéral';
