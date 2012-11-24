f8c094935289d64d229e4b27c530cc4e194ea842
Michael Göhler <somebody.here@gmx.de>
2012-11-24 23:38:47 +0100

    removed some spaces

--- templates/settings.php
+++ templates/settings.php
@@ -7,10 +7,10 @@
 			<?php echo $l->t('Username'); ?> <input type="text" name="user_subsonic"  id="user_subsonic"  value="<?php echo $_['user_subsonic']; ?>"  placeholder="<?php echo $l->t('Username'); ?>" /><br />
 			<?php echo $l->t('Password'); ?> <input type="password" name="pass_subsonic"  id="pass_subsonic"  value="<?php echo $_['pass_subsonic']; ?>"  placeholder="<?php echo $l->t('Password'); ?>" />
 			<strong>Headphones settings</strong><br />
-			<?php echo $l->t('URL'); ?> <input type="text" name="addr_headphones"  id="addr_headphones"  value="<?php echo $_    ['addr_headphones']; ?>"  placeholder="<?php echo $l->t('URL'); ?>"  style="width: 300px;"/><br />
-			<?php echo $l->t('Username'); ?> <input type="text" name="user_headphones"  id="user_headphones"  value="<?php echo $_['use    r_headphones']; ?>"  placeholder="<?php echo $l->t('Username'); ?>" /><br />
-			<?php echo $l->t('Password'); ?> <input type="password" name="pass_headphones"  id="pass_headphones"  value="<?php echo $_[    'pass_headphones']; ?>"  placeholder="<?php echo $l->t('Password'); ?>" /><br />
-			API-Key <input type="text" name="apikey_headphones"  id="apikey_headphones"  value="<?php echo $_['a    pikey_headphones']; ?>"  placeholder="<?php echo $l->t('API-Key'); ?>" style="width: 220px;"/>
+			<?php echo $l->t('URL'); ?> <input type="text" name="addr_headphones"  id="addr_headphones"  value="<?php echo $_['addr_headphones']; ?>"  placeholder="<?php echo $l->t('URL'); ?>"  style="width: 300px;"/><br />
+			<?php echo $l->t('Username'); ?> <input type="text" name="user_headphones"  id="user_headphones"  value="<?php echo $_['user_headphones']; ?>"  placeholder="<?php echo $l->t('Username'); ?>" /><br />
+			<?php echo $l->t('Password'); ?> <input type="password" name="pass_headphones"  id="pass_headphones"  value="<?php echo $_['pass_headphones']; ?>"  placeholder="<?php echo $l->t('Password'); ?>" /><br />
+			API-Key <input type="text" name="apikey_headphones"  id="apikey_headphones"  value="<?php echo $_['apikey_headphones']; ?>"  placeholder="<?php echo $l->t('API-Key'); ?>" style="width: 220px;"/>
 		</div>
 	</fieldset>
 </form>
